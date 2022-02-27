f8d55c3215e441a8158f7aac30cff02163c34c7b
comstering <coms1101@naver.com>
2022-02-27 11:04:26 +0900

    ✨ feat: Add function of add

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 class Hello:
     def say(self):
         print('Hello World!')
+    def add(self, x, y):
+        return x + y
 
 if __name__ == "__main__":
     h = Hello()
     h.say()
+    h.add(2, 3)
