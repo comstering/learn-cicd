311b5c6a229c907ede81006dfe820d2d2ec83fc5
comstering <coms1101@naver.com>
2022-02-27 11:20:28 +0900

    ✅ test: Update test code
    Use python unittest

--- test_hello.py
+++ test_hello.py
@@ -1,18 +1,20 @@
+import unittest
 import hello
 
-class TestHello():
-    def test_add(self):
+class TestHello(unittest.TestCase):
+    def test_add_positive(self):
         h = hello.Hello()
-        if h.add(2, 3) != 5:
-            print('FAILED')
-            exit(1)
-        print('PASSED')
-
-        if h.add(2, 5) != 7:
-            print('FAILED')
-            exit(1)
-        print('PASSED')
+        result = h.add(2, 3)
+        self.assertEqual(result, 5)
+        result = h.add(2, 7)
+        self.assertEqual(result, 9)
+    
+    def test_add_negative(self):
+        h = hello.Hello()
+        result = h.add(-2, 3)
+        self.assertEqual(result, 1)
+        result = h.add(-2, -7)
+        self.assertEqual(result, -9)
 
 if __name__ == '__main__':
-    t = TestHello()
-    t.test_add()
+    unittest.main()
